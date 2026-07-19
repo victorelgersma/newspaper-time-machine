e08d77a42cba9b3d28420d227b8553f702cb815e
Victor Elgersma <elgersmav@gmail.com>
2026-07-19 21:01:05 +0300

    stuff

--- html/partials/footer.php
+++ html/partials/footer.php
@@ -1,11 +1,7 @@
 
 <footer class="site-footer">
     <hr>
-    <p class="sans">
-        Created by <a href="https://vjbe.net/">V.J.B Elgersma </a>
-    </p>
     <p class="footer-links sans">
-        <a href="/">Home</a> • 
         <a href="/catalogue">Catalogue</a> • 
         <a href="/about">About & Contributing</a>
     </p>
